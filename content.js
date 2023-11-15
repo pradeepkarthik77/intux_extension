3ca623431d6e233255d090864efa6fb2b8edcdbc
pradeepkarthik77 <77573751+pradeepkarthik77@users.noreply.github.com>
2023-11-15 20:06:44 +0530

    Added dots clickers which change color when clicked  9 times

--- content.js
+++ content.js
@@ -36,6 +36,8 @@ function initGazer() {
 
                 // content-script.js
 
+                createDots();
+
                 overlayDiv.addEventListener('click', function(event) {
                         // Handle the click event
                         const clickCoordinates = {
@@ -45,6 +47,86 @@ function initGazer() {
                 
                         // Perform any necessary actions, such as updating the model
                         console.log('Clicked on coordinates:', clickCoordinates);
+
                 });
 
+
+
+}
+
+
+function createDots() {
+    // Define the number of dots and their colors
+    const numDots = 9;
+    const dotColors = ['red', 'yellow'];
+
+    for (let i = 0; i < numDots; i++) {
+        const dot = document.createElement('div');
+        dot.className = 'calibration-dot';
+        dot.style.position = 'absolute';
+        dot.style.width = '20px'; // Adjust the width and height as needed
+        dot.style.height = '20px';
+        dot.style.borderRadius = '50%'; // Make the dot round
+        dot.style.transition = 'background-color 0.3s'; // Add transition for color change
+
+        // Set initial click count to 0 for each dot
+        dot.clickCount = 0;
+
+        // Set dot color based on the number of clicks
+        dot.style.background = dotColors[0];
+
+        // Add the dot to the overlay
+        document.getElementById('calibration-overlay').appendChild(dot);
+
+        // Add click event listener to each dot
+        dot.addEventListener('click', function () {
+            handleDotClick(dot);
+        });
+    }
+
+    // Position the dots at the edges of the screen
+    positionDots();
+}
+
+function positionDots() {
+    const dots = document.getElementsByClassName('calibration-dot');
+    const numDots = dots.length;
+
+    // Set positions for each dot
+    const positions = [
+        { top: 0, left: 0 },           // Top-left
+        { top: 0, right: 0 },          // Top-right
+        { top: '50%', left: 0 },       // Middle-left
+        { top: '50%', right: 0 },      // Middle-right
+        { bottom: 0, left: 0 },        // Bottom-left
+        { bottom: 0, right: 0 },       // Bottom-right
+        { top: 0, left: '50%' },       // Top-center
+        { bottom: 0, left: '50%' },    // Bottom-center
+        { top: '50%', left: '50%' }    // Center
+    ];
+
+    for (let i = 0; i < numDots; i++) {
+        const dot = dots[i];
+        const position = positions[i];
+
+        // Apply position styles to each dot
+        Object.assign(dot.style, position);
+    }
+}
+
+let clickCount = 0; // Variable to track the number of clicks
+
+function handleDotClick(dot) {
+    dot.clickCount++;
+
+    // Change color after 10 clicks
+    if (dot.clickCount === 10) {
+        dot.style.background = 'yellow';
+
+        // Reset click count for the current dot
+        dot.clickCount = 0;
+    }
+
+    // Perform any additional actions needed for calibration
+    console.log('Dot clicked:', dot.style.background);
 }
