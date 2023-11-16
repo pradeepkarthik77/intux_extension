ed4e4587ea5bea7ad2c65cf9b97fdab72494fb0a
pradeepkarthik77 <77573751+pradeepkarthik77@users.noreply.github.com>
2023-11-16 23:35:46 +0530

    Added start calibration

--- content.js
+++ content.js
@@ -7,6 +7,16 @@ chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         if (request.message === 'enabledWebgazer') {
             initGazer();
+            createCalibration();
+        }
+    }
+);
+
+// content.js
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        if (request.message === 'enableCalibration') {
+            createCalibration();
         }
     }
 );
@@ -22,40 +32,41 @@ function initGazer() {
             webgazer.showVideoPreview(true) /* shows all video previews */
             .showPredictionPoints(true) /* shows a square every 100 milliseconds where current prediction is */
             .applyKalmanFilter(true); /* Ka*/
-
-                const overlayDiv = document.createElement('div');
-                overlayDiv.id = 'calibration-overlay';
-                document.body.appendChild(overlayDiv);
-
-                // Add styles to make the overlay transparent and cover the entire page
-                overlayDiv.style.position = 'fixed';
-                overlayDiv.style.top = '0';
-                overlayDiv.style.left = '0';
-                overlayDiv.style.width = '100%';
-                overlayDiv.style.height = '100%';
-                overlayDiv.style.background = 'rgba(0, 0, 0, 0.5)'; // Semi-transparent black background
-                overlayDiv.style.zIndex = '9999'; // Ensure the overlay is on top of other elements
-
-                // content-script.js
-
-                createDots();
-
-                overlayDiv.addEventListener('click', function(event) {
-                        // Handle the click event
-                        const clickCoordinates = {
-                        x: event.clientX,
-                        y: event.clientY
-                        };
-                
-                        // Perform any necessary actions, such as updating the model
-                        console.log('Clicked on coordinates:', clickCoordinates);
-
-                });
-
-
-
 }
 
+
+function createCalibration()
+{
+    
+    const overlayDiv = document.createElement('div');
+    overlayDiv.id = 'calibration-overlay';
+    document.body.appendChild(overlayDiv);
+
+    // Add styles to make the overlay transparent and cover the entire page
+    overlayDiv.style.position = 'fixed';
+    overlayDiv.style.top = '0';
+    overlayDiv.style.left = '0';
+    overlayDiv.style.width = '100%';
+    overlayDiv.style.height = '100%';
+    overlayDiv.style.background = 'rgba(0, 0, 0, 0.5)'; // Semi-transparent black background
+    overlayDiv.style.zIndex = '9999'; // Ensure the overlay is on top of other elements
+
+    // content-script.js
+
+    createDots();
+
+    overlayDiv.addEventListener('click', function(event) {
+            // Handle the click event
+            const clickCoordinates = {
+            x: event.clientX,
+            y: event.clientY
+            };
+    
+            // Perform any necessary actions, such as updating the model
+            console.log('Clicked on coordinates:', clickCoordinates);
+
+    });
+}
 
 function createDots() {
     // Define the number of dots and their colors
--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-var button = document.getElementById('sendMessageButton');
+var button = document.getElementById('startTrackingBtn');
 
 // Add a click event listener to the button
 button.addEventListener('click', function () {
@@ -6,5 +6,16 @@ button.addEventListener('click', function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         var activeTab = tabs[0];
         chrome.tabs.sendMessage(activeTab.id, { message: 'enabledWebgazer' });
+    });
+});
+
+var startCalibration = document.getElementById('startCalibrationBtn');
+
+// Add a click event listener to the button
+startCalibration.addEventListener('click', function () {
+    console.log("hello");
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        var activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, { message: 'enableCalibration' });
     });
 });
