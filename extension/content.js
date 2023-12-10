d6b8090c1b265029abed8360473b2d5fd8fbd2f1
pradeepkarthik77 <77573751+pradeepkarthik77@users.noreply.github.com>
2023-12-10 18:52:19 +0530

    added code to send data to options

--- extension/content.js
+++ extension/content.js
@@ -184,10 +184,10 @@ function moveTarget(iteration,direction,limit,container,elem,currentValue)
 
         if(iteration == 1)
         {
-            setTimeout(onStopCalibration,3000);
-            // delay(customTime).then(()=>{
-            //     moveTarget(iteration+1,"left",container.offsetWidth/2,container,elem,getCenterCoordinates(elem).x);
-            // })
+            // setTimeout(onStopCalibration,3000);  
+            delay(customTime).then(()=>{
+                moveTarget(iteration+1,"left",container.offsetWidth/2,container,elem,getCenterCoordinates(elem).x);
+            })
         }
 
         if(iteration == 2)
