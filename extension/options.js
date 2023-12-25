5f94211fc67e7554e1d54237efb5d5cbb3e34994
pradeepkarthik77 <77573751+pradeepkarthik77@users.noreply.github.com>
2023-12-25 11:18:20 +0530

    Small changes in videoURL

--- extension/options.js
+++ extension/options.js
@@ -12,26 +12,26 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-// // Add event listeners for range inputs
-// document.addEventListener('DOMContentLoaded', function () {
-//     const q2 = document.getElementById('q2slide');
-//     const q2val = document.getElementById('q2Value');
-//     q2.addEventListener('input', function () {
-//         q2val.textContent = this.value;
-//     });
+// Add event listeners for range inputs
+document.addEventListener('DOMContentLoaded', function () {
+    const q2 = document.getElementById('q2slide');
+    const q2val = document.getElementById('q2Value');
+    q2.addEventListener('input', function () {
+        q2val.textContent = this.value;
+    });
 
-//     const q4 = document.getElementById('q4slide');
-//     const q4val = document.getElementById('q4Value');
-//     q4.addEventListener('input', function () {
-//         q4val.textContent = this.value;
-//     });
+    const q4 = document.getElementById('q4slide');
+    const q4val = document.getElementById('q4Value');
+    q4.addEventListener('input', function () {
+        q4val.textContent = this.value;
+    });
 
-//     const q5 = document.getElementById('q5slide');
-//     const q5val = document.getElementById('q5Value');
-//     q5.addEventListener('input', function () {
-//         q5val.textContent = this.value;
-//     });
-// });
+    const q5 = document.getElementById('q5slide');
+    const q5val = document.getElementById('q5Value');
+    q5.addEventListener('input', function () {
+        q5val.textContent = this.value;
+    });
+});
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     if (message.message === "VideoURL") {
@@ -50,9 +50,9 @@ async function submitForm() {
 
     // Convert FormData to JSON
     const jsonData = {};
-    // formData.forEach((value, key) => {
-    //     jsonData[key] = value;
-    // });
+    formData.forEach((value, key) => {
+        jsonData[key] = value;
+    });
 
     // Loop through optionsPageData and add key-value pairs to jsonData
     for (const key in optionsPageData) {
